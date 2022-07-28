732a90ccce2d4b3505e0d732365c1ffe10961b98
SAM BHU <SAM BHU@DESKTOP-5MMOUET>
2022-07-28 10:09:03 +0545

    Add project files.

---
+++ Base_keyword_in_C_Sharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Base_keyword_in_C_Sharp
+{
+
+   public  class Animals
+    {
+        public String color = "Black";
+    }
+
+    public class Dog : Animals {
+
+        String color = "White";
+
+        public void showColor()
+        {
+            Console.WriteLine(base.color);
+            Console.WriteLine(color);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.showColor();
+        }
+    }
+}
